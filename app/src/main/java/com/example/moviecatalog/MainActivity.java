30c232b4505aeaf6c3838d5355ada6b342b09b14
Martin Bogatinoski <mbogatinoski@citycollege.sheffield.eu>
2023-04-15 21:30:21 +0200

    movie widget with api info on ui

--- app/src/main/java/com/example/moviecatalog/MainActivity.java
+++ app/src/main/java/com/example/moviecatalog/MainActivity.java
@@ -1,44 +1,49 @@
 package com.example.moviecatalog;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-import android.os.Bundle;
-import android.util.Log;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    private NavController navController;
-    private RequestQueue queue;
+    private List<Movie> movieList = new ArrayList<>();
+    private RecyclerView movieRecyclerView;
+    private MovieAdapter movieAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //volley api settings
-        queue = Volley.newRequestQueue(this);
-        String url = "https://app-vpigadas.herokuapp.com/api/movies/demo/";
+        movieRecyclerView = findViewById(R.id.recycler_view);
+        movieAdapter = new MovieAdapter(this, movieList);
 
-        //navigation settings
-        navController = Navigation.findNavController(this, R.id.nav_graph);
-        NavigationUI.setupActionBarWithNavController(this, navController);
+        movieRecyclerView.setAdapter(movieAdapter);
+        movieRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://app-vpigadas.herokuapp.com/api/movies/demo/";
 
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
                 (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
@@ -46,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
-                            JSONArray jsonArray = response.getJSONArray("movies");
+                            JSONArray jsonArray = response.getJSONArray("results");
                             for (int i = 0; i < jsonArray.length(); i++) {
                                 JSONObject movieObj = jsonArray.getJSONObject(i);
                                 int id = movieObj.getInt("id");
@@ -63,7 +68,8 @@ public class MainActivity extends AppCompatActivity {
 
                                 Movie movie = new Movie(id, title, overview, popularity, voteCount, voteAverage, releaseDate, poster);
 
-                                // TODO: Add the movie to your list or display it in your UI
+                                movieList.add(movie);
+                                movieAdapter.notifyDataSetChanged();
                             }
                         } catch (JSONException e) {
                             Log.e("MainActivity", "Error parsing JSON response", e);
@@ -75,15 +81,16 @@ public class MainActivity extends AppCompatActivity {
                     public void onErrorResponse(VolleyError error) {
                         Log.e("MainActivity", "Error fetching movie data", error);
                     }
-                });
+                }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+        };
 
-        // Add the request to the RequestQueue.
         queue.add(jsonObjectRequest);
-    }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        return NavigationUI.navigateUp(navController, new AppBarConfiguration.Builder(R.id.movieFragment).build())
-                || super.onSupportNavigateUp();
     }
 }
